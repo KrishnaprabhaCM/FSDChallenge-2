c508f51895dcc3f11a303512d06ac73f51f74f13
KrishnaprabhaCM <102539266+KrishnaprabhaCM@users.noreply.github.com>
2022-07-10 16:22:05 +0530

    version2

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SamplesComponent } from './pages/samples/samples.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'samples',component:SamplesComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'testimonials',component:TestimonialsComponent},
+  {path:'services',component:ServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { SamplesComponent } from './pages/samples/samples.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    TestimonialsComponent,
+    ServicesComponent,
+    SamplesComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
